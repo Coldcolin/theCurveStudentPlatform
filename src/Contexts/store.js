e0874a59326d409f7dc35ea5c1c323ab3e7970cd
HP PROBOOK <decorcecolin@gmail.com>
2024-08-26 11:29:19 +0100

    added dev tools settings

--- src/Contexts/store.js
+++ src/Contexts/store.js
@@ -13,13 +13,19 @@ import {
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
  
 // import rootReducer from './reducers'
- 
+
+// if (import.meta.env.VITE_NODE_ENV === 'production') {
+//   console.log("production")
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ = undefined;
+// }
+
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
 }
 const persistedReducer = persistReducer(persistConfig, myIdReducer)
+
 
 export const store = configureStore({
     reducer: { Id : persistedReducer },
@@ -29,6 +35,7 @@ export const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
+    devTools: import.meta.env.VITE_NODE_ENV !== 'production',
 })
 // const store = createStore(persistedReducer)
 export const Persistor = persistStore(store)
